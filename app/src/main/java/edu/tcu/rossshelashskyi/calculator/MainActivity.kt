aecc4611f655012f33cec3f730d59f255f0bea39
RosShelashskyi <ross.shelashskyi@tcu.edu>
2024-09-24 13:33:21 -0500

    First commit

---
+++ app/src/main/java/edu/tcu/rossshelashskyi/calculator/MainActivity.kt
@@ -0,0 +1,99 @@
+package edu.tcu.rossshelashskyi.calculator
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private var output: String = ""
+    private var afterOperator = false
+    private var firstDigitZero = false
+    private var hasDot = false
+    private var valueStatIndex = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val resultTv = findViewById<TextView>(R.id.result_tv)
+
+        findViewById<Button>(R.id.zero_btn).setOnClickListener{onDigit(resultTv, "0")}
+        findViewById<Button>(R.id.one_btn).setOnClickListener{onDigit(resultTv, "1")}
+        findViewById<Button>(R.id.two_btn).setOnClickListener{onDigit(resultTv, "2")}
+        findViewById<Button>(R.id.three_btn).setOnClickListener{onDigit(resultTv, "3")}
+        findViewById<Button>(R.id.four_btn).setOnClickListener{onDigit(resultTv, "4")}
+        findViewById<Button>(R.id.five_btn).setOnClickListener{onDigit(resultTv, "5")}
+        findViewById<Button>(R.id.six_btn).setOnClickListener{onDigit(resultTv, "6")}
+        findViewById<Button>(R.id.seven_btn).setOnClickListener{onDigit(resultTv, "7")}
+        findViewById<Button>(R.id.eight_btn).setOnClickListener{onDigit(resultTv, "8")}
+        findViewById<Button>(R.id.nine_btn).setOnClickListener{onDigit(resultTv, "9")}
+
+        findViewById<Button>(R.id.add_btn).setOnClickListener{onOperator(resultTv, "+")}
+        findViewById<Button>(R.id.subtract_btn).setOnClickListener{onOperator(resultTv, "-")}
+        findViewById<Button>(R.id.multiply_btn).setOnClickListener{onOperator(resultTv, "*")}
+        findViewById<Button>(R.id.divide_btn).setOnClickListener{onOperator(resultTv, "/")}
+
+        findViewById<Button>(R.id.equal_btn).setOnClickListener{onEqual()}
+        findViewById<Button>(R.id.dot_btn).setOnClickListener{onDot(resultTv)}
+        findViewById<Button>(R.id.clear_btn).setOnClickListener{onClear(resultTv)}
+
+    }
+    private fun onDigit(resultTv: TextView, digit: String){
+        if(digit == "0"){
+            if(firstDigitZero) return
+            if(output.isEmpty() || afterOperator){
+                firstDigitZero = true
+            }
+            output += digit
+            resultTv.text = output
+            afterOperator = false
+        }else{
+            if(firstDigitZero){
+                output = output.dropLast(1)
+            }
+            firstDigitZero = false
+            output += digit
+            resultTv.text = output
+            afterOperator = false
+        }
+    }
+
+    private fun onOperator(resultTv: TextView, operator: String){
+        if(afterOperator) return
+        output += operator
+        resultTv.text = output
+        afterOperator = true
+        firstDigitZero = false
+    }
+
+    private fun onDot(resultTv: TextView){
+        if(hasDot) return
+        output += "."
+        resultTv.text = output
+        hasDot = true
+        firstDigitZero = false
+    }
+
+    private fun onEqual(){
+        if(afterOperator) return
+    }
+
+    private fun onClear(resultTv: TextView){
+        output = "0"
+        resultTv.text = output
+        afterOperator = false
+        firstDigitZero = true
+        hasDot = false
+    }
+}
